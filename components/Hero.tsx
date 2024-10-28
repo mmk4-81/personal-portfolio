2c176182de750d896a69323023d82b607821e008
mmk4-81 <mmkkarbalaei@gmail.com>
2024-10-28 19:02:39 +0330

    add majic Button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -24,15 +26,22 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[80vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center ">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic web majic with nexy.js
+            Dynamic web majic with next.js
           </h2>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Consepts Into Seamless Expriences"
+            words="Transforming Consepts Into Seamless User Expriences"
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m MMK, a Next.js developer
             </p>
+            <a href="#about">
+              <MagicButton
+                title='Show my work'  
+                icon = {<FaLocationArrow />}
+                position="right"            
+              />
+            </a>
         </div>
       </div>
     </div>
