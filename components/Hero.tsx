ba8fd4361cd34cb6961e6b9f255681e642987afb
mmk4-81 <mmkkarbalaei@gmail.com>
2024-10-28 16:51:59 +0330

    modify text generate effect

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,6 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -24,13 +23,18 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[80vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center ">
-            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-              Dynamic web majic with nexy.js
-            </h2>
-            <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Transforming Consepts Into Seamless Expriences" />
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic web majic with nexy.js
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Consepts Into Seamless Expriences"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m MMK, a Next.js developer
+            </p>
         </div>
       </div>
-
     </div>
   );
 };
